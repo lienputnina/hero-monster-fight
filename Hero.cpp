bef4bde9b94a8d6a4c9a197c1b40ca4aee8183b8
lienputnina <liene.putnina20@gmail.com>
2024-06-06 09:43:41 +0300

    chore: updated comments

--- Hero.cpp
+++ Hero.cpp
@@ -7,14 +7,19 @@ Initializing a Hero instance with specific names for the inherited 'name' and
 constructor runs first. This way the inherited parameters are initialized
 correctly.
 */
+/*
+To initialize a Hero object, need to initialize a Character object first, since
+Hero object will also be a Character object.
+*/
 Hero::Hero(string heroName, int heroLife, int heroExperience)
     : Character(heroName, heroLife) {
   experience = heroExperience;
 };
 
+// Using Monster &monster as params to access Monster methods
 void Hero::Kill(Monster &monster) {
-  int monsterLife = monster.GetLife();
-  int monsterLevel = monster.GetLevel();
+  int monsterLife = monster.GetLife();   // comes from Character
+  int monsterLevel = monster.GetLevel(); // comes from Monster
 
   int heroExperienceBump = monsterLife * monsterLevel;
   monster.Hit(monsterLife);
